9b06c976eae26e76a96d97eaae58b8d3eccfea1a
Vijaykumar77 <degalavijaykumar77@gmail.com>
2022-06-09 20:26:06 +0530

    add changes

--- index.js
+++ index.js
@@ -138,10 +138,162 @@
 
 
 
-var sum=function (a,b)
-{
+// var sum=function (a,b)
+// {
 
-    console.log(a*b)
+//     console.log(a*b)
 
-}
-sum(20,5)
+// }
+// sum(20,5)
+
+//Impure function :Modifying an outer variable is nothing but impure function i.e its having an side effect 
+
+// var x=10;
+
+// function add()
+
+// {
+// console.log(x)
+// x++;
+
+// }
+// add()
+// add()
+// add()
+// add()
+// add()
+
+
+//pure function:
+
+
+// function add(x)
+// {
+
+// console.log(x+1)
+
+
+// }
+
+// add(12)
+// add(12)
+// add(12)
+// add(12)
+// add(12)
+
+
+//copy homework 
+//do home work 
+
+
+// function do_homework(x)
+// {
+// x()
+//     console.log("do homwork")
+// }
+
+
+// function copy_homework()
+// {
+
+//     console.log("copy homwork")
+// }
+
+
+// do_homework(copy_homework)
+
+// function* myGen()
+// {
+
+// yield "a"  //a is nothing but yieldd expression
+// yield "b"
+// yield "c"
+
+
+// }
+// const store=myGen()
+// console.log(store.next().value)
+// console.log(store.next())
+// console.log(store.next())
+// console.log(store.next())
+// // console.log("A is paused and outer cosole is getting into an action ")
+//  //next: to execute the value 
+
+
+// function outer()
+// {
+
+//     var a = "outer Variable "
+    
+//       function innerfunction(){
+//              console.log(a)
+
+//     }
+
+// return innerfunction
+// }
+
+// const store=outer()
+// store()
+
+
+// function add(a,b,c)
+// {
+
+
+// return a+b+c
+
+// }
+// const store=add(2,5,7)
+// console.log(store)
+
+// function add(a){
+
+// return function (b){
+
+// return function (c){
+
+
+
+//     return a+b+c;
+// }
+
+// }
+
+
+// }
+
+// let res=add(2)(4)(6)
+// console.log(res)
+// // console.log(res)
+// // let data=res(4)
+// // console.log(data)
+// // let data1=data(20)
+// // console.log(data1)
+
+
+// const store=new Map()
+// obj={}
+// store.set(obj,100)
+// console.log(store)
+
+
+
+// const arr=[100,200,300,400,500]
+
+// let returnvalue=arr.map((x)=>x+1)
+
+// console.log(arr)
+// console.log(returnvalue)
+
+
+
+const arr=[100,200,300,400,500]
+
+
+let returnvalue=arr.forEach(num =>
+    console.log(`${num}x${num}=${num*num}`))
+
+console.log(arr)
+console.log(returnvalue)
+
